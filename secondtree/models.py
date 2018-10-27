c0aefde1fd57e0fc29620ef76a214422973766ff
Papp Endre <endi.papp@outlook.com>
2018-10-27 13:09:37 +0200

    test commit

--- secondtree/models.py
+++ secondtree/models.py
@@ -2,3 +2,15 @@ from django.db import models
 
 class Post(models.Model):
 	text = models.CharField(max_length=1000)
+
+class User(models.Model):
+	user = models.CharField(max_length=20)
+	password = models.CharField(max_length=20)
+
+class ItemDet(models.Model):
+	image = models.ImageField()
+	position = models.IntegerField(max_length=100)
+	description = models.CharField(max_length=500)
+	price = models.IntegerField()
+	category = models.CharField(max_length=50)
+	name = models.CharField(max_length=100)
