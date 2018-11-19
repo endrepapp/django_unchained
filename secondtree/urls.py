37a9dd4bdb8cb8ff174a4f9061454794694d9e3d
Papp Endre <endi.papp@outlook.com>
2018-11-19 14:23:18 +0100

    [2018.11.19]CSS modify - Overall site upgrade - LOGO update - Header update [2018.11.19-20] next Update -Main Site items changes -Items site visual update -Check box updates

--- secondtree/urls.py
+++ secondtree/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
-    path('<int:ItemDet_id>/', views.detail, name='detail'),
+	path('', views.index, name='index'), # main site load
+    path('<int:ItemDet_id>/', views.detail, name='detail'), # adott oldalt nyitja meg részletes leírással.
     
 ]
--- secondtree/views.py
+++ secondtree/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import Http404
 from .models import ItemDet
-
+#Lekérdezi az adatbázisból a tárgyakat
 def index(request): 
 	all_itemdets = ItemDet.objects.all()
 	return render(request, 'sitee/index.html', {'all_itemdets':all_itemdets})
 
 
-
+# X tárgyat nyitja meg ami a fő oldalt kivan listázva.
 def detail(request, ItemDet_id):
 	try:
 		item = ItemDet.objects.get(pk=ItemDet_id)
